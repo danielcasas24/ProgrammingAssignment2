603d110a789f258318e596c8b8c3f3a1cde43fde
Daniel Casas <danielcasas24@hotmail.com>
2020-08-10 23:11:53 +0200

    feat: functions in test mode

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,58 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Pair of functions tO cache the inverse of a matrix
 
-## Write a short comment describing this function
+
+## cacheSolve: This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) m <<- inverse
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
+  ## Return a matrix that is the inverse of 'x'
 }
+
+# R program to find inverse of a Matrix 
+
+# Create 3 different vectors 
+# using combine method. 
+a1 <- c(3, 2, 5) 
+a2 <- c(2, 3, 2) 
+a3 <- c(5, 2, 4) 
+A <- rbind(a1, a2, a3) 
+
+# print the original matrix 
+print(A) 
+
+# Use the solve() function  
+# to calculate the inverse. 
+T1 <- solve(A) 
+
+# print the inverse of the matrix. 
+print(T1) 
+
+T2 <- cacheSolve(makeCacheMatrix(A))
+
+print(T2)
