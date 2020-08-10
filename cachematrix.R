c6df65ef52d0638aa97da01b2a8092887d2ef277
Daniel Casas <danielcasas24@hotmail.com>
2020-08-10 23:17:04 +0200

    style: clean code to send coursera

--- cachematrix.R
+++ cachematrix.R
@@ -31,28 +31,4 @@ cacheSolve <- function(x, ...) {
   m <- solve(data, ...)
   x$setinverse(m)
   m
-  ## Return a matrix that is the inverse of 'x'
 }
-
-# R program to find inverse of a Matrix 
-
-# Create 3 different vectors 
-# using combine method. 
-a1 <- c(3, 2, 5) 
-a2 <- c(2, 3, 2) 
-a3 <- c(5, 2, 4) 
-A <- rbind(a1, a2, a3) 
-
-# print the original matrix 
-print(A) 
-
-# Use the solve() function  
-# to calculate the inverse. 
-T1 <- solve(A) 
-
-# print the inverse of the matrix. 
-print(T1) 
-
-T2 <- cacheSolve(makeCacheMatrix(A))
-
-print(T2)
